c4e4f69583255342eb6d8dcf7b2a3c9c52c19d14
ArchieCall <archcall@gmail.com>
2017-09-02 16:22:59 -0400

    0422

--- src/get_threshold.jl
+++ src/get_threshold.jl
@@ -12,17 +12,10 @@ function get_threshold()
   PkgName = "\\AccurateSleep"
   SubFolderPath = "\\src\\"
   FileName = "sleep_threshold.jl"
-  #@show(PkgDir)
-  #@show(PkgName)
-  #@show(SubFolderPath)
-  #@show(FileName)
   FullPath1 = PkgDir * PkgName * SubFolderPath * FileName
-  #@show(FullPath1)
-  #FullPath1 = "C://Users//Owner//.julia//v0.6//AccurateSleep//src//sleep_threshold.jl"
   if isfile(FullPath1)
     #println("found default")
     threshold = include(FullPath1)
-    #@show(threshold)
   else
     println("default not found")
   end
